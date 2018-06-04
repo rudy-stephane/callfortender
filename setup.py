f63e84991681e6fb06d013f9e317280a3b5a7b49
rudy-stephane <tekamfossi@gmail.com>
2018-06-04 15:18:58 +0100

    ajout du fichier app.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Example-CherryPy',
+      version='1.0',
+      description='OpenShift CherryPy Example',
+      author='Rudy Stephane',
+      author_email='tekamfossi@yahoo.fr',
+      install_requires=['CherryPy>=3.2.4']
+)
